660f1810278bbf1df2786185be009ab4dcea3e1b
ReevesRobotics <ravenrobotics.frc@gmail.com>
2026-02-03 18:47:50 -0600

    but i have and idea

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -101,6 +101,7 @@ public class RobotContainer {
   
    public void updateOdometry() {
     m_robotDrive.periodic();
+    LimelightHelpers.SetRobotOrientation("limelight", m_robotDrive.m_poseEstimator.getEstimatedPosition().getRotation().getDegrees(), 0, 0, 0, 0, 0);
     limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("");
     m_robotDrive.m_poseEstimator.addVisionMeasurement(
        limelightMeasurement.pose,
