f2532f07a8d69187ce67609393b761f75b783ff5
ReevesRobotics <ravenrobotics.frc@gmail.com>
2026-02-14 08:25:43 -0600

    ballarina mode activated

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -117,7 +117,7 @@ public class RobotContainer {
     // This method loads the auto when it is called, however, it is recommended
     // to first load your paths/autos when code starts, then return the
     // pre-loaded auto/path
-    return new PathPlannerAuto("Example Auto");
+    return new PathPlannerAuto("test");
   }
 
   public void refresh_shuffleboard() {
