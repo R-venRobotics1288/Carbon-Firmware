5d11247d092255e6c4497ada4210e96c751495f0
ReevesRobotics <ravenrobotics.frc@gmail.com>
2026-03-21 09:54:18 -0500

    split variable shoot command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,6 +25,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 // import frc.robot.commands.VariableShootCommand;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.HopperConstants;
+import frc.robot.commands.AutoAimCommand;
 import frc.robot.commands.VariableShootCommand;
 import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
@@ -53,10 +54,12 @@ public class RobotContainer {
 
   Trigger startButton = new JoystickButton(m_driverController, XboxController.Button.kStart.value);
   Trigger intakeButton = new JoystickButton(m_operatorController, XboxController.Button.kRightBumper.value);
-  Trigger shootButton = new Trigger(() -> m_operatorController.getRightTriggerAxis() > 0.8);
+  Trigger variableShootButton = new Trigger(() -> m_operatorController.getRightTriggerAxis() > 0.8);
   Trigger dumpButton = new Trigger(() -> m_operatorController.getLeftTriggerAxis() > 0.8);
   Trigger clearJamButton = new JoystickButton(m_operatorController, XboxController.Button.kLeftBumper.value);
-  Trigger variableShootButton = new JoystickButton(m_driverController, XboxController.Button.kX.value);
+  Trigger shootButton = new JoystickButton(m_operatorController, XboxController.Button.kB.value);
+  Trigger autoAimButton = new JoystickButton(m_driverController, XboxController.Button.kX.value);
+
   // Trigger climbButton = new JoystickButton(m_operatorController, XboxController.Button.kLeftBumper.value);
   // Trigger cancelButton = new JoystickButton(m_operatorController, XboxController.Button.kRightBumper.value);
 
@@ -133,11 +136,12 @@ public class RobotContainer {
     }, m_robotDrive));
 
     intakeButton.whileTrue(m_hopper.shootCommand(HopperConstants.kIntakeFlywheelMotorSpeed, HopperConstants.kIntakeFeederMotorSpeed, 0.0));
-    shootButton.whileTrue(m_hopper.shootCommand(HopperConstants.kShooterFlywheelMotorSpeed, HopperConstants.kShooterFeederMotorSpeed, 1.0)); //TODO: tune delay!!
+    variableShootButton.whileTrue(m_hopper.shootCommand(HopperConstants.kShooterFlywheelMotorSpeed, HopperConstants.kShooterFeederMotorSpeed, 1.0)); //TODO: tune delay!!
+    shootButton.whileTrue(new VariableShootCommand(m_hopper, m_robotDrive));
     dumpButton.whileTrue(m_hopper.shootCommand(HopperConstants.kReverseIntakeFlywheelMotorSpeed, HopperConstants.kReverseIntakeFeederMotorSpeed, 0.0));
     clearJamButton.whileTrue(m_hopper.shootCommand(HopperConstants.kReverseIntakeFlywheelMotorSpeed, 0.0, 5.0));
+    autoAimButton.whileTrue(new AutoAimCommand(m_robotDrive));
 
-    variableShootButton.whileTrue(new VariableShootCommand(m_hopper, m_robotDrive));
     // climbButton.onTrue(climbCommand);
     // cancelButton.onTrue(Commands.runOnce(climbCommand::cancel));
 }
